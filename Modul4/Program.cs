7bb5461ae1e7788302bb2a9bfa0e274e54065772
Lvsnflwrs <helmyfariq@gmail.com>
2024-03-16 09:00:27 +0700

    menambahkan class PosisiKarakterGame

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -15,6 +15,77 @@
     }
 }
 
+public class PosisiKarakterGame {
+    enum State { Berdiri, Terbang, Jongkok, Tengkurap };
+    public static void gas()
+    {
+        State state = State.Berdiri;
+        string[] screenName = { "Berdiri", "Terbang", "Jongkok", "Tengkurap" };
+        while (true)
+        {
+            Console.WriteLine("Sekarang sedang " + screenName[(int)state]);
+            Console.Write("Enter Command : ");
+
+            string command = Console.ReadLine();
+            switch (state)
+            {
+                case State.Berdiri:
+                    if (command == "W")
+                    {
+                        state = State.Terbang;
+                    } else if (command == "S")
+                    {
+                        state = State.Jongkok;
+                    } else
+                    {
+                        state = State.Berdiri;
+                        Console.WriteLine("Posisi StandBy");
+                    }
+                    break;
+                case State.Terbang:
+                    if (command == "X")
+                    {
+                        state = State.Jongkok;
+                    } else if (command == "S")
+                    {
+                        state = State.Berdiri;
+                        Console.WriteLine("Posisi Standby");
+                    } else
+                    {
+                        state = State.Jongkok;
+                    }
+                    break;
+                case State.Jongkok:
+                    if (command == "W")
+                    {
+                        state = State.Berdiri;
+                        Console.WriteLine("Posisi StandBy");
+                    } else if (command == "S")
+                    {
+                        state = State.Tengkurap;
+                        Console.WriteLine("Posisi Istirahat");
+                    } else
+                    {
+                        state = State.Jongkok;
+                    }
+                    break;
+                case State.Tengkurap:
+                    if(command == "W")
+                    {
+                        state = State.Jongkok;
+                    } else
+                    {
+                        state = State.Tengkurap;
+                        Console.WriteLine("Posisi Istirahat");
+                    }
+                    break; 
+            }
+        }
+    }
+}
+
+
+
 public class Program
 {
     static void Main(string[] args)
@@ -33,6 +104,8 @@ public class Program
         {
             Console.WriteLine("buah tidak valid");
         }
+        PosisiKarakterGame posisi = new PosisiKarakterGame();
+        PosisiKarakterGame.gas();
     }
 }
 
